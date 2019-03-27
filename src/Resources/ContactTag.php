d60dcb1cfb63853bffebb46cbc29e2b59ffb426c
sgrootveld <sgrootveld@gmail.com>
2019-03-27 16:07:14 +0100

    Remove Contact Tags

---
+++ src/Resources/ContactTag.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace ByTestGear\ActiveCampaign\Resources;
+
+class ContactTag extends Resource
+{
+    /**
+     * The id of the contact tag.
+     *
+     * @var int
+     */
+    public $id;
+
+    /**
+     * Contact ID.
+     *
+     * @var int
+     */
+    public $contact;
+
+    /**
+     * Tag ID.
+     *
+     * @var int
+     */
+    public $tag;
+}
